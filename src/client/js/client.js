7affbdfbf039abc158f185649b7785da5b7752fa
Brian Leathem <bleathem@gmail.com>
2015-01-19 14:55:02 -0800

    Created a multi-user chat app

--- src/client/js/client.js
+++ src/client/js/client.js
@@ -1,20 +1,52 @@
 (function(vertx) {
-  var eb = new vertx.EventBus('http://localhost:8080/eventbus');
+  var eventBus = new vertx.EventBus('/eventbus')
+    , user
+    , token;
 
-  var chat = document.getElementById('chat');
-  var message = document.getElementById('message');
-  var button = document.getElementById('submit');
+  var loginFormListener = function(e) {
+    e.preventDefault();
+    xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(e) {
+      if (xhr.readyState == 4 && !token) {
+        token = JSON.parse(xhr.responseText).token;
+        document.getElementById('loginPanel').style.display = 'none';
+        document.getElementById('chatPanel').style.display = 'block';
+      }
+    }
+    var username = document.getElementById('username');
+    if (username.value) {
+      user = username.value;
+      xhr.open("POST","/api/user",true);
+      xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+      xhr.send('user=' + username.value);
+    }
+    return false;
+  }
+  document.getElementById('loginForm').addEventListener('submit', loginFormListener);
 
-  eb.onopen = function() {
-    eb.registerHandler('demo-chat', function(message) {
-      console.log('received a message: ' + JSON.stringify(message));
-      var p = document.createElement("p");
-      p.textContent = message;
-      chat.appendChild(p);
-    });
-    button.addEventListener('click', function() {
-      eb.publish('demo-chat', message.value);
-      message.value = '';
-    });
+  var chatSendListener = function(e) {
+    e.preventDefault();
+    var message = document.getElementById('message');
+    var data = {
+      token: token,
+      user: user,
+      message: message.value
+    }
+    eventBus.publish('demo-chat', data);
+    message.value = '';
+    return false;
+  };
+  document.getElementById('chatForm').addEventListener('submit', chatSendListener);
+
+  var chatResponseHandler = function(data) {
+    var p = document.createElement('p');
+    p.textContent = data.message;
+    p.className = data.user === user ? 'right' : 'left';
+    var messages = document.getElementById('messages');
+    messages.appendChild(p);
+    messages.scrollTop = messages.scrollHeight;
+  };
+  eventBus.onopen = function() {
+    eventBus.registerHandler('demo-chat', chatResponseHandler);
   }
 }(vertx));
--- src/server/server.js
+++ src/server/server.js
@@ -4,11 +4,15 @@ var vertx = require('vertx')
   , container = require('vertx/container')
   , console = require('vertx/console');
 
-var server = vertx.createHttpServer();
+var server = vertx.createHttpServer()
+  , token = 'HDWWER$^BBE#$552323dfxcsd3@!134#$';
 
 var routeMatcher = new vertx.RouteMatcher();
-routeMatcher.get('/api', function(req) {
-  req.response.end('My API');
+routeMatcher.post('/api/user', function(req) {
+  var data = {
+    token : token
+  };
+  req.response.end(JSON.stringify(data));
 });
 var staticHandler = new org.vertx.mods.web.StaticFileHandler(__jvertx, 'src/client', 'src/client/index.html', false, false);
 routeMatcher._to_java_handler().noMatch(staticHandler);
@@ -16,10 +20,17 @@ server.requestHandler(routeMatcher);
 
 var sockJSServer = vertx.createSockJSServer(server);
 sockJSServer.bridge({prefix : '/eventbus'},
-  [{address: 'demo-chat'}],
+  [
+    {
+      address: 'demo-chat',
+      match: {
+        token: token
+      }
+    }
+  ],
   [{address: 'demo-chat'}]
 );
 
-server.listen(8080, 'localhost');
+server.listen(9000, '0.0.0.0');
 
 module.exports = server;
